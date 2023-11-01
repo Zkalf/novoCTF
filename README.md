aa6a9d2c81b0bf3ac750fb692c5f36fa09df22fe
Michael <do-mic@hotmail.com>
2023-11-01 11:44:06 +0100

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Welcome to Novo Nordisk capture the flag
+You will be presented with exercises from different fields in computer security with varying degree of difficulty.\
+One exercise is not dependent on another and you can choose to solve the exercises in any order.\
+The goal is to find strings of text called flags which are purposely hidden.\
+These exercise will increase your problem solving ability, knowledge-base and technical skills.
+
+The structure of a flag is in the format such as:\
+novoCTF{RandomWordsNumbers_123}
+
+## General information
+* Exercises are distributed into folders
+* Only one flag per exercise
+* Hints are available as .txt files
+* Solve exercises in any order
+* Exercise 1-7 are category easy
+* Exercise 8-10 are category medium
+* Exercise 11-12 are category hard
+
+## Python
+Some of the exercises require Python.\
+If you can't install Python on your PC, then use [Google Colaboratory](https://colab.research.google.com/).
+
+## Submit flags
+Reach out to MCBQ on Teams or Outlook
+
+### Contact
+MCBQ
+
+[Hidden Flag 0 - Surely nobody will look inside the readme]: #
+[novoCTF{VeryHiddenZero_000}]: #
---
+++ flag10/secret_program.py
@@ -0,0 +1,45 @@
+# Hiding this really important string in an obscure piece of code is brilliant!
+# We want our company to know the information is safe.
+novo_secret = "?@G@r%uLq62Cr92C86%6==:?80deaN"
+
+# Reference alphabet
+alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ \
+            "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+def decode_secret(secret):
+    """ROT47 decode
+    NOTE: encode and decode are the same operation in the ROT cipher family.
+    """
+
+    # Encryption key
+    rotate_const = 47
+
+    # Storage for decoded secret
+    decoded = ""
+
+    # decode loop
+    for c in secret:
+        index = alphabet.find(c)
+        original_index = (index + rotate_const) % len(alphabet)
+        decoded = decoded + alphabet[original_index]
+
+    print(decoded)
+
+def choose_greatest():
+    """Echo the largest of the two numbers given by the user to the program
+    Warning: this function was written quickly and needs proper error handling
+    """
+
+    user_value_1 = input("What's your first number? ")
+    user_value_2 = input("What's your second number? ")
+    greatest_value = user_value_1 # need a value to return if 1 & 2 are equal
+
+    if user_value_1 > user_value_2:
+        greatest_value = user_value_1
+    elif user_value_1 < user_value_2:
+        greatest_value = user_value_2
+
+    print( "The number with largest positive magnitude is "
+        + str(greatest_value) )
+
+choose_greatest()
